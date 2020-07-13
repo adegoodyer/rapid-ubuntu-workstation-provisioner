9d7eca58ec67612a107a41ede13159825e0096fa
adegoodyer <adriangoodyer@gmail.com>
2020-07-13 08:30:08 +0100

    added backup GNOME preferences function

--- system/terminal.sh
+++ system/terminal.sh
@@ -19,3 +19,15 @@ function install_byobu {
   sudo apt install byobu -y
   print_completion
 }
+
+# Backing up, resetting and restoring GNOME Terminal preferences using dconf
+# https://askubuntu.com/questions/967517/backup-gnome-terminal
+
+# Backup GNOME terminal settings
+function backup_terminal_preferences {
+  print_heading "Backing up GNOME Terminal preferences"
+  dconf dump /org/gnome/terminal/ > ~/gnome_terminal_settings_backup.txt
+  print_info "Backed up to ~/gnome_terminal_settings_backup.txt"
+  print_completion
+}
+
