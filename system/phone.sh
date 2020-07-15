281f2eac7d6f38cb7e9c3c5ddb90b8b7eb1671a7
adegoodyer <adriangoodyer@gmail.com>
2020-07-15 11:21:04 +0100

    enhanced install gsconnect function

--- system/phone.sh
+++ system/phone.sh
@@ -6,8 +6,21 @@
 # Tested OS:        Ubuntu 20.04
 
 # Install GSConnect
+# https://www.omgubuntu.co.uk/2018/11/connect-android-ubuntu-gsconnect
 function install_gsconnect {
+    # Enable universe repository if not enabled
+    print_heading "Enabling Universe repository"
+    sudo add-apt-repository -u universe -y
+
+    # Install GSConnect
     print_heading "Installing GSConnect"
     sudo apt install gnome-shell-extension-gsconnect -y
+
+    # Enable browser support (Firefox/Chromium)
+    # print_heading "Enabling browser support (Firefox/Chromium)"
+    # sudo apt install gnome-shell-extension-gsconnect-browsers
+
     print_completion
+    print_info "You now need to install the KDE Connect app on your phone https://play.google.com/store/apps/details?id=org.kde.kdeconnect_tp&hl=en_GB"
+    print_user_prompt "Its important this is done first and the connection is instigated from your phone to the PC and not vice-versa"
 }
