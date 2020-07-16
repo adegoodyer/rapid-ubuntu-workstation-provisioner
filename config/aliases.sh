1eb7f3c9a11ebf69e685b7b15997b92995fb2bef
adegoodyer <adriangoodyer@gmail.com>
2020-07-16 15:43:12 +0100

    fixed function to add one unique log line per file and added aliases

---
+++ config/aliases.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+IFS=$'\n'
+
+# Author:           Ade Goodyer
+# Created:          15/07/2020
+# Description:      Add aliases to BASH in one handy shell script
+
+#
+append_unique_line 'alias c="clear"' ~/.bashrc
+
+# Git
+append_unique_line 'alias gs="clear && git status"' ~/.bashrc
+append_unique_line 'alias gl="clear && git log --oneline"' ~/.bashrc
+
+print_info "Your additional aliases have been set. View with the alias command"
--- helpers/essential_functions.sh
+++ helpers/essential_functions.sh
@@ -9,5 +9,5 @@
 # https://stackoverflow.com/questions/3557037/appending-a-line-to-a-file-only-if-it-does-not-already-exist
 function append_unique_line {
     # grep -qxF 'include "/configs/projectname.conf"' foo.bar || echo 'include "/configs/projectname.conf"' >> foo.bar
-    grep -qxF $1 $2 || echo $1 >> $2
+    grep -qsxF $1 $2 || echo $1 >> $2
 }
